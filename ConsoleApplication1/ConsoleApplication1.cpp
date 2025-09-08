70f18e0df05bb09a0846b5724205d5e1a920b0f8
Fronees <anbo.you77@gmail.com>
2025-09-08 17:39:46 +0300

    ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,10 +1,39 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-    
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int ptr;
+	cout << "(1-налево, 2-направо, 3-вперед, 4-назад)";
 
+	cin >> ptr;
 
+	switch (ptr)
+	{
+	case 1:
+		cout << "Встретил дракона(проиграл)" << endl;
+		break;
+
+	case 2:
+		cout << "Нашел клад" << endl;
+		break;
+
+	case 3:
+		cout << "Вышел к реке" << endl;
+		break;
+
+	case 4:
+		cout << "Вернулся к началу" << endl;
+		break;
+
+	case 5:
+		cout << "Победил дракона" << endl;
+		break;
+	}
+
+	return 0;
 }
